751ce3d4569211b7e07c4c5d6ea573a63e12691a
Boris Barac <boris.raptor@gmail.com>
2022-10-19 22:44:32 +0200

    helpers module added

--- RMApp/Package.swift
+++ RMApp/Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let TCA: Target.Dependency = .product(name: "ComposableArchitecture", package: "swift-composable-architecture")
 let services: Target.Dependency = "Services"
+let helpers: Target.Dependency = "Helpers"
 let rmGql: Target.Dependency = .product(name: "RMGraphQL", package: "RMGraphQL")
 
 let package = Package(
@@ -24,6 +25,8 @@ let package = Package(
         .library(
             name: "HomePage",
             targets: ["HomePage"]),
+        .library(name: "Helpers",
+                 targets: ["Helpers"])
     ],
     dependencies: [
         .package(url: "https://github.com/pointfreeco/swift-composable-architecture.git", .upToNextMajor(from: "0.43.0")),
@@ -34,6 +37,7 @@ let package = Package(
             name: "AppCore",
             dependencies: [
                 TCA,
+                helpers,
             ]),
         .testTarget(
             name: "AppCoreTests",
@@ -53,9 +57,18 @@ let package = Package(
             dependencies: [
                 TCA,
                 services,
+                helpers,
             ]),
         .testTarget(
             name: "HomePageTests",
             dependencies: ["HomePage"]),
+
+        .target(
+            name: "Helpers",
+            dependencies: [
+            ]),
+        .testTarget(
+            name: "HelpersTests",
+            dependencies: ["Helpers"]),
     ]
 )
---
+++ RMApp/Tests/HelpersTests/HelpersTests.swift
@@ -0,0 +1,7 @@
+import XCTest
+@testable import Helpers
+
+final class HelpersTests: XCTestCase {
+    func testExample() throws {
+    }
+}
