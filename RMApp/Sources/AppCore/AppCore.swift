2d69c3d97815c007379b3b0bc18e41de353d7587
Boris Barac <boris.raptor@gmail.com>
2022-10-19 23:09:31 +0200

    AppCore connected to App Content

--- RMApp/Sources/AppCore/AppCore.swift
+++ RMApp/Sources/AppCore/AppCore.swift
@@ -1,6 +1,20 @@
-public struct RMApp {
-    public private(set) var text = "Hello, World!"
+import SwiftUI
 
+public struct AppCoreView: View {
     public init() {
+    }
+
+    public var body: some View {
+        VStack {
+            Text("Hello, world!")
+            Text("Hello, Hello, Hello!!!")
+        }
+        .padding()
+    }
+}
+
+struct AppCoreView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppCoreView()
     }
 }
--- RickAndMortyApp/ContentView.swift
+++ RickAndMortyApp/ContentView.swift
@@ -10,13 +10,8 @@ import AppCore
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        AppCoreView()
+            .padding()
     }
 }
 
