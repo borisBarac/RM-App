082a1e789357e044ff81a472616315f83a0aaeb7
Boris Barac <boris.raptor@gmail.com>
2022-10-20 22:43:50 +0200

    Fake Service added

--- RMApp/Sources/Services/Services.swift
+++ RMApp/Sources/Services/Services.swift
@@ -1,5 +1,31 @@
-import RMGraphQL
 
-public struct Services {
+import Foundation
+import RMGraphQL
+import ComposableArchitecture
+import Helpers
+import Combine
+
+public class RMRepository {
+    public typealias RMEffect = AnyPublisher<[String], Never>
+
+    public func repositoryEffect() -> RMEffect {
+        Just(["f1, f2, f3"]).eraseToAnyPublisher()
+    }
+
+    public func dummyRepositoryEffect() -> RMEffect {
+        repositoryEffect()
+    }
+
 }
 
+extension RMRepository: DependencyKey {
+    public static let liveValue = RMRepository()
+    public static let testValue = RMRepository()
+}
+
+public extension DependencyValues {
+    var rmRepository: RMRepository {
+        get { self[RMRepository.self] }
+        set { self[RMRepository.self] = newValue }
+    }
+}
