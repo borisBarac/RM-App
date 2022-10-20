0b5eab91eb0e5cf70696f05d68907083ec2333f6
Boris Barac <boris.raptor@gmail.com>
2022-10-20 19:41:06 +0200

    DatailPage - small init

--- RMApp/Sources/DetailsPage/DetailsPage.swift
+++ RMApp/Sources/DetailsPage/DetailsPage.swift
@@ -1,3 +1,19 @@
-public struct DetailsPage {
+import SwiftUI
+
+public struct DetailPageView: View {
+    public init() {
+    }
+
+    public var body: some View {
+        VStack {
+            Text("Hello, DetailPage")
+        }
+        .padding()
+    }
 }
 
+struct DetailPageView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailPageView()
+    }
+}
