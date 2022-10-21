ad8f3e0f021502a8f2e44742194383bc19f1b8aa
Boris Barac <boris.raptor@gmail.com>
2022-10-22 01:22:15 +0200

    Colors helper added

---
+++ RMApp/Sources/Helpers/Colors.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+
+public struct Theme {
+    @Environment(\.colorScheme) var colorScheme
+    private var isDark: Bool {
+        colorScheme == .dark
+    }
+
+    public var backgroundColor: Color {
+        isDark ? .black : .white
+    }
+
+    public var textColor: Color {
+        isDark ? .white : .black
+    }
+
+}
+
+public extension UIColor {
+    func inverse () -> UIColor {
+        var r:CGFloat = 0.0; var g:CGFloat = 0.0; var b:CGFloat = 0.0; var a:CGFloat = 0.0;
+        if self.getRed(&r, green: &g, blue: &b, alpha: &a) {
+            return UIColor(red: 1.0-r, green: 1.0 - g, blue: 1.0 - b, alpha: a)
+        }
+        return .black // Return a default colour
+    }
+}
