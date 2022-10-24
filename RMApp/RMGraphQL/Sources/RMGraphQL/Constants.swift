a545ca1fe66aaf67bbeafe912c9076136c2ba6ae
Boris Barac <boris.raptor@gmail.com>
2022-10-24 23:30:34 +0200

    RMError conforms to LocalizedError

--- RMApp/RMGraphQL/Sources/RMGraphQL/Constants.swift
+++ RMApp/RMGraphQL/Sources/RMGraphQL/Constants.swift
@@ -3,8 +3,31 @@ import Foundation
 // fallback endpoint if not provided in config
 let rmApiEndpont = URL(string: "https://rickandmortyapi.graphcdn.app/")!
 
-public enum RMError: Error {
+public enum RMError: Error, LocalizedError {
     case asyncConvertionFailed
     case couldNotCreateSqlCache
     case graphQlError(String)
+
+    public var errorDescription: String? {
+        switch self {
+        case .asyncConvertionFailed:
+            return "GraphQL query could not be converted to ASYNC"
+        case .couldNotCreateSqlCache:
+            return "RM API could not create SQL cache"
+        case .graphQlError(let message):
+            return message
+        }
+
+    }
+
+    public var failureReason: String? {
+        switch self {
+        case .asyncConvertionFailed:
+            return "GraphQL query could not be converted to ASYNC"
+        case .couldNotCreateSqlCache:
+            return "RM API could not create SQL cache"
+        case .graphQlError(let message):
+            return message
+        }
+    }
 }
