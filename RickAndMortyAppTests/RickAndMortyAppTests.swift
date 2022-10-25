4894467e80beeceef808f0d6c926667b4c655f80
Boris Barac <boris.raptor@gmail.com>
2022-10-25 16:52:12 +0200

    Workspace created

--- RickAndMortyAppTests/RickAndMortyAppTests.swift
+++ RickAndMortyAppTests/RickAndMortyAppTests.swift
@@ -1,11 +1,5 @@
-//
-//  RickAndMortyAppTests.swift
-//  RickAndMortyAppTests
-//
-//  Created by Boris Barac on 19.10.2022.
-//
-
 import XCTest
+@testable import RickAndMortyApp
 
 final class RickAndMortyAppTests: XCTestCase {
 
@@ -18,18 +12,7 @@ final class RickAndMortyAppTests: XCTestCase {
     }
 
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+        XCTAssertTrue(true)
     }
 
 }
