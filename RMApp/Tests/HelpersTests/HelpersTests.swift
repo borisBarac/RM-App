6c1fda22b5007e1d3c84ac01fa4a4b1b63fe8599
Boris Barac <boris.raptor@gmail.com>
2022-11-05 15:08:03 +0100

    SmallRoundedTextViewForTags: Snapshot test added

--- RMApp/Tests/HelpersTests/HelpersTests.swift
+++ RMApp/Tests/HelpersTests/HelpersTests.swift
@@ -4,11 +4,21 @@ import SnapshotTesting
 import XCTest
 @testable import Helpers
 
+/*
+ We can scale this approach super easy to other custom views (cells, headers and so),
+ especially because we are using UIModels in all the places
+ */
+
 #if os(iOS)
 final class HelpersTests: XCTestCase {
     func testLoadingViewImage() throws {
         let vc = UIHostingController(rootView: LoadingView())
         assertSnapshot(matching: vc, as: .image(size: CGSize(width: 250, height: 250)))
+    }
+
+    func testTagText() throws {
+        let vc = UIHostingController(rootView: SmallRoundedTextViewForTags(text: "TAG TEXT GOES HERE"))
+        assertSnapshot(matching: vc, as: .image(size: CGSize(width: 100, height: 30)))
     }
 
 }
